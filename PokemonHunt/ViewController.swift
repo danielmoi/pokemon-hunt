e3320a9c072280d6a791608f4628e960c85726ac
danielmoi <daniel@ampersandmoi.com>
2017-12-29 12:01:37 +1100

    Hook up user location

--- PokemonHunt/ViewController.swift
+++ PokemonHunt/ViewController.swift
@@ -7,18 +7,33 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print("READY TO GO!")
+            mapView.showsUserLocation = true
+        } else {
+            manager.requestWhenInUseAuthorization()
+        }
+        
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
