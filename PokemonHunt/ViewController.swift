8a743ce44505db1b09240ff606a87e398c1d7dee
danielmoi <daniel@ampersandmoi.com>
2017-12-29 12:11:24 +1100

    Center and zoom

--- PokemonHunt/ViewController.swift
+++ PokemonHunt/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
     
     var manager = CLLocationManager()
+    var updateCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +25,32 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
             print("READY TO GO!")
             mapView.showsUserLocation = true
+            
+            manager.startUpdatingLocation()
         } else {
             manager.requestWhenInUseAuthorization()
         }
         
+        
+        
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        /*
+         centerCoordinate: The center point of the new coordinate region.
+         latitudinalMeters: The amount of north-to-south distance (measured in meters) to use for the span.
+         longitudinalMeters: The amount of east-to-west distance (measured in meters) to use for the span.
+        */
+        
+        // stop moving map when user moves = allow panning
+        if updateCount < 3 {
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 1000, 1000)
+            
+            // false so it's smooth upon opening
+            mapView.setRegion(region, animated: false)
+            updateCount += 1
+        }
         
         
         
